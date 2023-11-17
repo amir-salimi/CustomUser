a84bddf6d92d06a1227e8f014feca93c5cc82c6e
Amirhosseinsalimi123 <amir0905salimi@gmail.com>
2023-11-17 22:11:41 +0330

    custom user for phone and email created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    phone = models.PositiveBigIntegerField(null=False, unique=True)
+    email = models.EmailField(null=False)
+    
+    class Meta:
+        ordering = ['date_joined']
+
+    def __str__(self):
+        return self.username
